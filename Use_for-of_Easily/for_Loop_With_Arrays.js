a5451304fb8371da661f036ab09e07cf9838865a
MaHi <muhammadShefain@gmail.com>
2023-12-04 13:08:50 +0600

    use for loop more easily on array

---
+++ Use_for-of_Easily/for_Loop_With_Arrays.js
@@ -0,0 +1,16 @@
+const array = [1, 2, 3];
+
+/**
+ * Object.entries(array) @returns index and value paris in @Array
+ */
+
+console.log(Object.entries(array)); // --> [ [0,1], [1,2], [2,3] ]
+
+/**
+ * Next we distracture the array
+ * Now we have index and val
+ */
+
+for (const [index, val] of Object.entries(array)) {
+  console.log(index, val);
+}
