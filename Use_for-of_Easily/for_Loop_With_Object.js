bde5b744e2c559e70c2c47e199bb27617029a409
MaHi <muhammadShefain@gmail.com>
2023-12-06 23:38:32 +0600

    fix some code

---
+++ Use_for-of_Easily/for_Loop_With_Object.js
@@ -0,0 +1,21 @@
+const profile = {
+  name: 'mahi ',
+  age: 18,
+};
+
+/**
+ * Object.entries(profile) @return key value pairs in @Array
+ */
+console.log(Object.entries(profile)); // --> [ [name, "mahi" ], ["age" , 18 ] ]
+
+/**
+ * Next we  destructure the array
+ * now we have key and val
+ */
+
+for (const [key, val] of Object.entries(profile)) {
+  console.log(key, val);
+}
+
+
+
